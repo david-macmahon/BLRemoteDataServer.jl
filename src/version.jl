ef74d2fa1bb03360753ccfe744b6d168de2f58aa
David MacMahon <davidm@astro.berkeley.edu>
2023-08-06 14:40:12 -0400

    Fix typo

--- src/version.jl
+++ src/version.jl
@@ -1,7 +1,7 @@
 # Version number of current module.  Adapted from
 # https://github.com/JuliaDocs/Documenter.jl/blob/c0cfded/src/Documenter.jl#L25-L32
 const VERSION = let
-    # Project.toml must be one directry above module's .jl file
+    # Project.toml must be one directory above module's .jl file
     project = joinpath(dirname(dirname(pathof(@__MODULE__))), "Project.toml")
     Base.include_dependency(project) # Retrigger precompilation when Project.toml changes
     toml = read(project, String)
